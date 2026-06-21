2797b6bbe01a66211a6376798c6b9489a65b569e
Syariffullah <dinnereatc@gmail.com>
2026-06-21 14:33:33 +0700

    feat: add Testimonial section with GSAP animations and integrate into App component

--- src/components/VideoPin.tsx
+++ src/components/VideoPin.tsx
@@ -15,7 +15,6 @@ export default function VideoPin() {
           start: "-15% top",
           end: "200% top",
           scrub: 1.5,
-          markers: true,
           pin: true,
         },
       });
